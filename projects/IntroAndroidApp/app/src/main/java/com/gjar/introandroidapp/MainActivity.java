7e86bf217c52fb04efb27846983ac2f1241e4e3c
Jan Lenko <lenkojan@gmail.com>
2016-09-12 21:15:51 +0200

    Added IntroAndroidApp project

---
+++ projects/IntroAndroidApp/app/src/main/java/com/gjar/introandroidapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.gjar.introandroidapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(MainActivity.this, FinishActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
