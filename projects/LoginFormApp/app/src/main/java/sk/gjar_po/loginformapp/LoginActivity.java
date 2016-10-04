37e1baa59fd908fcb2d6e7f9e0141f6f2b0a2d9e
Ján Lenko <lenkojan@gmail.com>
2016-10-04 11:09:27 +0200

    Added comments to sources

--- projects/LoginFormApp/app/src/main/java/sk/gjar_po/loginformapp/LoginActivity.java
+++ projects/LoginFormApp/app/src/main/java/sk/gjar_po/loginformapp/LoginActivity.java
@@ -7,25 +7,36 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+// definicia triedy pre LoginActivity
 public class LoginActivity extends AppCompatActivity {
 
-    EditText mLoginTextView;
-    EditText mPasswordTextView;
+    // odkaz na editacne pole na zadanie prihlasovacieho mena
+    EditText mLoginEditText;
+    // odkaz na editacne pole na zadanie hesla
+    EditText mPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // nastavenie uzivatelskeho rozhrania definovaneho v activity_login.xml
         setContentView(R.layout.activity_login);
-        mLoginTextView = (EditText) findViewById(R.id.login);
-        mPasswordTextView = (EditText) findViewById(R.id.password);
+        // nastavenia odkazov na editacne polia pre login a password
+        mLoginEditText = (EditText) findViewById(R.id.login);
+        mPasswordEditText = (EditText) findViewById(R.id.password);
+        // definicia akcie volanej pri stlaceni tlacidla s priradenym Id button
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String login = mLoginTextView.getText().toString();
-                String password = mPasswordTextView.getText().toString();
-                if (!login.equals("") && !password.equals("")) {
+                // ziskanie textu z editacneho pola pre login
+                String login = mLoginEditText.getText().toString();
+                // ziskanie textu z editacneho pola pre password
+                String password = mPasswordEditText.getText().toString();
+                // overenie ci login a password boli vyplnene
+                if (!"".equals(login) && !"".equals(password)) {
+                    // ak ano tak zavolaj metodu dispatchSuccessIntent s vyplnenym login a password
                     dispatchSuccessIntent(login, password);
                 } else {
+                    // ak nie tak zobraz hlasku
                     Toast.makeText(LoginActivity.this, "Please provide a login and password", Toast.LENGTH_LONG).show();
                 }
             }
@@ -33,12 +44,17 @@ public class LoginActivity extends AppCompatActivity {
 
     }
 
+    // metoda na odoslanie dat obrazovke SuccessActivity
     private void dispatchSuccessIntent(String login, String password) {
+        // vytvorenie systemoveho volania
         Intent intent = new Intent(this, SuccessActivity.class);
+        // vytvorenie a naplnenie objektu na prenos dat
         Bundle b = new Bundle();
         b.putString(SuccessActivity.ARG_LOGIN, login);
         b.putString(SuccessActivity.ARG_PASSWORD, password);
+        // priradenie dat systemovemu volaniu
         intent.putExtras(b);
+        // odoslanie systemoveho volania
         startActivity(intent);
     }
 }
--- projects/LoginFormApp/app/src/main/java/sk/gjar_po/loginformapp/SuccessActivity.java
+++ projects/LoginFormApp/app/src/main/java/sk/gjar_po/loginformapp/SuccessActivity.java
@@ -4,26 +4,37 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.EditText;
 
+// definicia triedy pre SuccessActivity
 public class SuccessActivity extends AppCompatActivity {
 
     public static final String ARG_LOGIN = "login";
     public static final String ARG_PASSWORD = "password";
 
+    // odkaz na zobrazovacie pole na zobrazenie prihlasovacieho mena
     EditText mLoginTextView;
+    // odkaz na zobrazovacie pole na zobrazenie prihlasovacieho hesla
     EditText mPasswordTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // nastavenie uzivatelskeho rozhrania definovaneho v activity_success.xml
         setContentView(R.layout.activity_success);
+        // nastavenia odkazu na zobrazenie pola pre login
         mLoginTextView = (EditText) findViewById(R.id.login);
+        // znefunkcnenie editacnej funkcionality
         mLoginTextView.setKeyListener(null);
+        // nastavenia odkazu na zobrazenie pola pre login
         mPasswordTextView = (EditText) findViewById(R.id.password);
+        // znefunkcnenie editacnej funkcionality
         mPasswordTextView.setKeyListener(null);
+        // zistenie ci boli poslane data cez systemove volanie
         if (getIntent().getExtras() != null) {
+            // zistenie ci bol poslany login a nastavenie poslanej hodnoty do widgetu mLoginTextView
             if (getIntent().getExtras().containsKey(ARG_LOGIN)) {
                 mLoginTextView.setText(getIntent().getExtras().getString(ARG_LOGIN));
             }
+            // zistenie ci bolp poslane heslo a nastavenie poslanej hodnoty do widgetu mPasswordTextView
             if (getIntent().getExtras().containsKey(ARG_PASSWORD)) {
                 mPasswordTextView.setText(getIntent().getExtras().getString(ARG_PASSWORD));
             }
