da3b9bfc78a28e657c528c1ea6a6dd6ad5728474
Jan Lenko <lenkojan@gmail.com>
2017-11-05 11:54:42 +0100

    Example of onClick button property

--- projects/FirstProject/app/src/main/java/com/lenkojan/firstproject/MainActivity.java
+++ projects/FirstProject/app/src/main/java/com/lenkojan/firstproject/MainActivity.java
@@ -1,7 +1,10 @@
 package com.lenkojan.firstproject;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClean(View v) {
+        ((TextView) findViewById(R.id.textView1)).setText("");
+    }
+
+    public void onAdd(View v) {
+        String currentText = ((TextView) findViewById(R.id.textView1)).getText().toString();
+        String newText = ((EditText) findViewById(R.id.editText1)).getText().toString() + "\n" + currentText;
+        ((EditText) findViewById(R.id.editText1)).setText("");
+        ((TextView) findViewById(R.id.textView1)).setText(newText);
     }
 }
